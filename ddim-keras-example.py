2eca2e352ef9166c0c0a70676a7c00558646f29f
Tom Zhao <zhaoxuan0914@hotmail.com>
2023-02-04 11:21:24 +0000

    added oxford flowers and segment without mask folders

--- ddim-keras-example.py
+++ ddim-keras-example.py
@@ -13,9 +13,9 @@ from keras import layers
 
 # %%
 # data
-dataset_name = "luna16_roi" # "oxford_flowers102"
+dataset_name = "luna16_raw" # "oxford_flowers102"
 dataset_repetitions = 5
-num_epochs = 2  # train for at least 50 epochs for good results
+num_epochs = 50  # train for at least 50 epochs for good results
 image_size = 512
 # KID = Kernel Inception Distance, see related section
 kid_image_size = 75
@@ -33,7 +33,7 @@ widths = [32, 64, 96, 128]
 block_depth = 2
 
 # optimization
-batch_size = 64
+batch_size = 4
 ema = 0.999
 learning_rate = 1e-3
 weight_decay = 1e-4
